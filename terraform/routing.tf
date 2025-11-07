a0cfc70e26e990266f49af7fac6dfda14f7d9b5c
Minh Huy <minhhuy3042004@gmai.com>
2025-11-07 21:32:15 +0700

    Add gitignore

--- terraform/routing.tf
+++ terraform/routing.tf
@@ -6,7 +6,6 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
-
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
 
@@ -16,16 +15,11 @@ resource "aws_route_table" "public_rt" {
   }
 
   tags = {
-    Name = "public-route-table"
+    Name = "rt-public"
   }
 }
 
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.public_a.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_route_table_association" "b" {
-  subnet_id      = aws_subnet.public_b.id
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public_rt.id
 }
